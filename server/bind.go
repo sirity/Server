5196574ac6cc0bfa963bbf168f8a951b53de045d
ben <ben@sirity.com>
2014-08-25 15:18:41 +0800

    reset password

--- server/bind.go
+++ server/bind.go
@@ -12,6 +12,7 @@ func bind() {
     mymux.HandleFunc("/user/set_password", setPassword)
     mymux.HandleFunc("/user/set_profile", setProfile)
     mymux.HandleFunc("/user/get_profile", getProfile)
+    mymux.HandleFunc("/user/reset_password/", resetPassword)
 
 
 
